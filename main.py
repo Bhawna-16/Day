2a639c2bd51bfaa9ccd1abf01cb1ff8934dbeba0
bhawna-16 <bhawnadikshit916@gmail.com>
2025-05-07 23:14:17 +0530

    first_commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.markdown("<h1 style='text-align: center; color: teal'><i>Oye Sun!</i></h1>", unsafe_allow_html=True)
+st.image("img.jpg")
+st.markdown("<h1 style='text-align: center; color: brown'><i>happy wala birthday to you!</i></h>", unsafe_allow_html=True)
+st.markdown("----")
+# col1, col2 = st.columns(2)
+# col1.markdown("<h3 style='color: teal;'><i>Thank You ❤️</i></h3>", unsafe_allow_html=True)
+# col2.image("pic.jpg")
